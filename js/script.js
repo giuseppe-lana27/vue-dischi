d72c4db85cbaf4caee0904a28a7774a04b3a2e39
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-04-08 15:42:57 +0200

    stampati a schermo i vari album

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 var app = new Vue({
-  el: 'app',
+  el: '#app',
   data: {
     albums: [],
   },
+  mounted: function(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((risposta) =>{
+      //console.log(risposta.data.response);
+      this.albums = risposta.data.response;
+    });
+  }
 
-})
+});
