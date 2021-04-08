eb60d43b5f99474c1a240220850fa1d8e994e3ea
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-04-08 16:12:17 +0200

    inserito foreach per pushare i generi inseriti nella select

--- js/script.js
+++ js/script.js
@@ -2,12 +2,21 @@ var app = new Vue({
   el: '#app',
   data: {
     albums: [],
+    genres: [],
   },
   mounted: function(){
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((risposta) =>{
       //console.log(risposta.data.response);
       this.albums = risposta.data.response;
+      // bonus 1 inserisco i generi in array da inserire nella select
+      this.albums.forEach((item) => {
+        if(!this.genres.includes(item.genre)){
+          this.genres.push(item.genre);
+        }
+        //console.log(this.genres);
+      });
+
     });
   }
 
